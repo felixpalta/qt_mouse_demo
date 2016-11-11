6bc96e18243bc63239e59749a873986505a97262
Felix Palta (office) <fpalta90@gmail.com>
2016-11-11 15:06:59 +0300

    Enable mouse tracking for all children of MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,17 +8,22 @@ MainWindow::MainWindow(QWidget *p) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    centralWidget()->setMouseTracking(true);
+
+    // Enable mouse tracking for all children widgets of this MainWindow.
+
+    QList<QWidget *> widgets =  findChildren<QWidget*>();
+    for (auto w : widgets) {
+        w->setMouseTracking(true);
+    }
+
     setMouseTracking(true);
+
 }
 
 void MainWindow::mouseMoveEvent(QMouseEvent * evt)
 {
     ui->textScreenPosX->setText(QString::number(evt->globalX()));
     ui->textScreenPosY->setText(QString::number(evt->globalY()));
-
-    ui->textWidgetPosX->setText(QString::number(evt->x()));
-    ui->textWidgetPosY->setText(QString::number(evt->y()));
 
     ui->textWindowPosX->setText(QString::number(qRound(evt->windowPos().x())));
     ui->textWindowPosY->setText(QString::number(qRound(evt->windowPos().y())));
